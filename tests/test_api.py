1bcde0397dd3d4d8d7c5f66ad48e84b8d92daf59
Dave McPherson <davemcpherson@wochstudios>
2022-01-27 23:15:12 -0500

    adding test case for list

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,5 +6,10 @@ def test_add_task():
     api.sync_api.sync()
 
 
+def test_list_task():
+    tasks = api.list_tasks("Inbox")
+    assert tasks is not None
+
+
 def test_delete_task():
     api.delete_task("Add Task from PyTest")
